35ed8cb13af91210c153f3ac97cf8ca7eece2702
Pedro Fernandez Munoz <pedrofm@pedrofm.dev>
2024-10-26 14:49:36 +0200

    parse data

---
+++ get_data.ts
@@ -0,0 +1,63 @@
+import { parse } from "@std/csv";
+import type { empresa } from "./main.ts";
+
+export async function getData(filePath: string): Promise<empresa[]> {
+    let textContent: string = await Deno.readTextFile(filePath);
+
+    const stringsToReplace: string[] = [
+        "Nombre",
+        "Número de teléfono",
+        "Dirección de correo electrónico",
+        "Empresa",
+        "MESAS REDONDAS. ¿A cual vas a asistir? ",
+        "¿Que buscas en el B2b?",
+        "¿Qué ofreces en el B2b?",
+        "¿Te apuntas al Brunch Networking?",
+        "Proporcione información adicional que nos pueda ser útil para organizar el B2B y el Brunch. *Número de asistente al Brunch* Horario límite del B2B*....",
+    ];
+
+    const newStrings: string[] = [
+        "nombre",
+        "telefono",
+        "correo",
+        "empresa",
+        "mesas_redondas",
+        "buscas",
+        "ofreces",
+        "brunch_networking",
+        "informacion_adicional",
+    ];
+
+    for (let i = 0; i < stringsToReplace.length; i++) {
+        const pattern = stringsToReplace[i]
+            .replace(/\?/g, "\\?")
+            .replace(/\./g, "\\.")
+            .replace(/\*/g, "\\*")
+            .replace(/\s/g, "\\s")
+            .replace(/\u00A0/g, "\\s");
+
+        const regex = new RegExp(pattern, "g");
+        textContent = textContent.replace(regex, newStrings[i]);
+    }
+
+    const dataParsed = parse(textContent, {
+        skipFirstRow: true,
+        strip: true,
+    });
+
+    const empresas: empresa[] = dataParsed.map((row): empresa => {
+        return {
+            nombre: row.nombre,
+            numeroTelefono: row.telefono,
+            direccionCorreo: row.correo,
+            empresa: row.empresa,
+            mesasRedondas: row.mesas_redondas.split(";").filter((item) => item.trim() !== "" ),
+            busca: row.buscas.split(";").filter((item) => item.trim() !== ""),
+            ofrece: row.ofreces.split(";").filter((item) => item.trim() !== ""),
+            seApuntaAlBrunch: row.brunch_networking,
+            infoAdicional: row.informacion_adicional,
+        };
+    });
+
+    return empresas;
+}
--- main.ts
+++ main.ts
@@ -1,59 +1,25 @@
-import Excel from "exceljs";
 import * as path from "@std/path";
+import { getData } from "./get_data.ts";
 
-type empresa = {
+export type empresa = {
     nombre: string;
     numeroTelefono: string;
     direccionCorreo: string;
     empresa: string;
-    mesasRedondas: string;
-    busca: string;
-    ofrece: string;
+    mesasRedondas: string[];
+    busca: string[];
+    ofrece: string[];
     seApuntaAlBrunch: string;
     infoAdicional: string;
 };
 
 const filePath: string = path.resolve(
-    "./sensitive-data/excel.xlsx",
+    "./sensitive-data/excel.csv",
 );
 
-function getCellValue(row: Excel.Row, cellIndex: number) {
-    const cell = row.getCell(cellIndex);
-
-    return cell.value ? cell.value.toString() : "";
-}
-
 async function main() {
-    const workbook = new Excel.Workbook();
-
-    console.warn(filePath);
-    const content = await workbook.xlsx.readFile(filePath);
-
-    const worksheet = content.worksheets[0];
-    const rowStartIndex = 1;
-    const numberOfRows = worksheet.rowCount - 1;
-
-    const rows = worksheet.getRows(rowStartIndex, numberOfRows) ?? [];
-
-    const empresas: empresa[] = rows.map((row): empresa => {
-        return {
-            nombre: getCellValue(row, 6),
-            numeroTelefono: "",
-            direccionCorreo: "",
-            empresa: "",
-            mesasRedondas: "",
-            busca: "",
-            ofrece: "",
-            seApuntaAlBrunch: "",
-            infoAdicional: "",
-        };
-    });
-
-    console.log(empresas);
+    const data: empresa[] = await getData(filePath);
+    console.log(data);
 }
-
-/**
- * Run CLI.
- */
 
 main();
